5f65429cb9f3f820a616d8d3cf97fed6fe6536db
ArzilRiyaj <arzilnishad@gmail.com>
2021-09-11 02:05:52 +0530

    done

--- first.php
+++ first.php
@@ -1,36 +1,17 @@
 <?php
 include_once "header.php";
 ?>
-  <nav class="navbar sticky-top bg-transparent navbar-expand-lg " style="background-image: url('assets/images/sketch-header.png'); background-repeat: no-repeat; background-size: 100% 100%;">
-  
-  <a class="navbar-brand" href="#"><img src="assets/images/Untitled design (6).png" class=" "width="80" alt="Logo"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-    
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="my-2 my-lg-0">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Oder Now</button>
-    </form>
-  </div>
-</nav>
-<div class="intro"style="padding-top: -100px; >
+<div class="col"  >
+
+<?php
+
+include_once "nav.php";
+?>
 
 
 
-<div id="myCarousel" class="carousel slide" data-bs-ride="carousel" ">
+<div id="myCarousel" class="carousel slide" data-bs-ride="carousel" >
     
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -52,7 +33,7 @@ include_once "header.php";
         </div>
       </div>
       <div class="carousel-item">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="assets/images/photo-1574085733277-851d9d856a3a.jpg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+      <img src="assets\images\photo-1574085733277-851d9d856a3a.jpg" class="d-block w-100" alt="...">
 
         <div class="container">
           <div class="carousel-caption">
@@ -63,7 +44,7 @@ include_once "header.php";
         </div>
       </div>
       <div class="carousel-item">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
+      <img src="assets\images\photo-1574085733277-851d9d856a3a.jpg" class="d-block w-100" alt="...">
 
         <div class="container">
           <div class="carousel-caption text-end">
@@ -128,9 +109,18 @@ include_once "header.php";
       <span class="subheading">MENU</span>
       <!-- <h2 class="mb-3">Our Latest Services</h2> -->
     <!-- </div> -->
+    <div id="myBtnContainer">
+  <button class="btn active" onclick="filterSelection('all')"> Show all</button>
+  <button class="btn" onclick="filterSelection('cars')"> Cars</button>
+  <button class="btn" onclick="filterSelection('animals')"> Animals</button>
+  <button class="btn" onclick="filterSelection('fruits')"> Fruits</button>
+  <button class="btn" onclick="filterSelection('colors')"> Colors</button>
+</div>
 
+ 
 
-    <div class="card" style="width: 18rem;">
+<div class="container">
+<div class=" filterDiv cars card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
@@ -138,7 +128,20 @@ include_once "header.php";
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
-
+  <div class="filterDiv cars">BMW</div>
+  <div class="filterDiv colors fruits">Orange</div>
+  <div class="filterDiv cars">Volvo</div>
+  <div class="filterDiv colors">Red</div>
+  <div class="filterDiv cars animals">Mustang</div>
+  <div class="filterDiv colors">Blue</div>
+  <div class="filterDiv animals">Cat</div>
+  <div class="filterDiv animals">Dog</div>
+  <div class="filterDiv fruits">Melon</div>
+  <div class="filterDiv fruits animals">Kiwi</div>
+  <div class="filterDiv fruits">Banana</div>
+  <div class="filterDiv fruits">Lemon</div>
+  <div class="filterDiv animals">Cow</div>
+</div>
 
 
 </div>
@@ -147,3 +150,102 @@ include_once "header.php";
 <?php
 include_once "footer.php";
 ?>
+<style>
+
+.container {
+  overflow: hidden;
+}
+
+.filterDiv {
+  float: left;
+  /* background-color: #2196F3; */
+  /* color: #ffffff; */
+  width: 100px;
+  line-height: 100px;
+  text-align: center;
+  margin: 2px;
+  display: none; /* Hidden by default */
+}
+
+/* The "show" class is added to the filtered elements */
+.show {
+  display: block;
+}
+
+/* Style the buttons */
+/* .btn {
+  border: none;
+  outline: none;
+  padding: 12px 16px;
+  background-color: #f1f1f1;
+  cursor: pointer;
+}
+
+/* Add a light grey background on mouse-over */
+/* .btn:hover {
+  background-color: #ddd;
+} */
+
+/* Add a dark background to the active button */
+/* .btn.active {
+  background-color: #666;
+  color: white;
+} */ 
+
+</style>
+
+
+
+<script>
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+
+
+</script>
