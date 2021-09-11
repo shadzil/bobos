9d491a17772f432c5a499c217521bbc54a751303
ArzilRiyaj <arzilnishad@gmail.com>
2021-09-11 10:28:51 +0530

    paralox

--- header.php
+++ header.php
@@ -32,7 +32,21 @@
  
 
 <!-- fonts -->
+<style>
+.parallax {
+  /* The image used */
+  background-image: url("assets/images/body-bg.jpg");
 
+  /* Set a specific height */
+  min-height: 500px; 
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
 
 
   </head>
