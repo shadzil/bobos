9ad942eeb482e8cdfb9cc968bbfc6ddf1669164f
ArzilRiyaj <arzilnishad@gmail.com>
2021-09-13 13:06:07 +0530

    done

--- Untitled-1.php
+++ Untitled-1.php
@@ -1,236 +1,121 @@
-<?php
-include_once "header.php";
-?>
 
 
-<?php
-
-include_once "nav.php";
-?>
-
-
-<!-- -----------------------Carousel------------------------------------------------------------------------------------------------------ -->
-<div id="myCarousel" class="carousel slide " data-bs-ride="carousel">
-
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-
-    <div class="carousel-item active">
-
-      <img src="assets\images\photo-1574085733277-851d9d856a3a.jpg" class="d-block w-100" alt="...">
-
-      <div class="container">
-        <div class="carousel-caption text-start">
-          <h1>Example headline.</h1>
-          <p>Some representative placeholder content for the first slide of the carousel.</p>
-          <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
-        </div>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="assets\images\photo-1574085733277-851d9d856a3a.jpg" class="d-block w-100" alt="...">
-
-      <div class="container">
-        <div class="carousel-caption">
-          <h1>Another example headline.</h1>
-          <p>Some representative placeholder content for the second slide of the carousel.</p>
-          <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
-        </div>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="assets\images\photo-1574085733277-851d9d856a3a.jpg" class="d-block w-100" alt="...">
-
-      <div class="container">
-        <div class="carousel-caption text-end">
-          <h1>One more for good mea78ooo78o7sure.</h1>
-          <p>Some representative placeholder content for the third slide of this carousel.</p>
-          <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-
-
-<!-- --------------Why Choose Us---------------------------------------------------------------------------------------------- -->
-
-
-<div class="container-fluid parallax  my-5 text-center px-5 p-3 text-white ">
-
-
+<div id="menu" class="container-fluid ">
   <div class=" text-center heading-section ftco-animate">
-    <p class=" brushfont">Why Choose Us</p>
-  </div>
-
-
-  <!-- Example row of fjgnjfcolumns -->
-  <div class="row square-wrapper">
-
-
-    <div class="col-md-4 square square-transition " data-aos="zoom-in" data-aos-offset="200" data-aos-delay="50" data-aos-duration="1000" data-aos-easing="ease-in-out" data-aos-mirror="true" data-aos-once="false">
-      <img src="assets\images\eco-logo.png" class="d-block w-25 mx-auto" alt="...">
-      <h2>Heading</h2>
-      <p>Donec id elit non mi porta gravida at eget metus., ut fermentum massa justo sit amet risus. Donec sed odio dui. </p>
-    </div>
-
-
-
-    <div class="col-md-4 square square-transition" data-aos="zoom-in" data-aos-offset="200" data-aos-delay="50" data-aos-duration="1000" data-aos-easing="ease-in-out" data-aos-mirror="true" data-aos-once="false">
-      <img src="assets\images\fresh.png" class="d-block w-25 mx-auto" alt="...">
-      <h2>Heading</h2>
-      <p>Donec id elit non mi porta mollis euismod. Donec sed odio dui. </p>
-    </div>
-
-
-    <div class="col-md-4 square square-transition" data-aos="zoom-in" data-aos-offset="200" data-aos-delay="50" data-aos-duration="1000" data-aos-easing="ease-in-out" data-aos-mirror="true" data-aos-once="false">
-      <img src="assets\images\quality.png" class="d-block w-25 mx-auto" alt="...">
-      <h2>Heading</h2>
-      <p>Donec sed odio dui. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-
-    </div>
-  </div>
-
-
-</div>
-<!-- ------------MENU------------------------------------------------------------------------------- -->
-
-
-
-<div  id="menu" class="container-fluid " >
-<div class=" text-center heading-section ftco-animate">
     <p class=" brushfont">Menu</p>
+
+
+
+    <div id="myBtnContainer">
+      <button class="btn active" onclick="filterSelection('all')"> Show all</button>
+      <button class="btn" onclick="filterSelection('bpots')"> Biriyani Pots</button>
+      <button class="btn" onclick="filterSelection('lpack')"> Lunch Pack</button>
+      <button class="btn" onclick="filterSelection('mutt')"> Mutton Masala</button>
+      <button class="btn" onclick="filterSelection('bev')"> Bevarages</button>
+    </div>
+
+  </div>
+
+  <!-- The filterable elements. Note that some have multiple class names (this can be used if they belong to multiple categories) -->
+
+
   
+      <div class="filterDiv bpots">
+        <div class="card bpots " style="width: 18rem;">
+          <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
+          <div class="card-body ">
+            <h4> Biriyani </h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+            <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="filterDiv colors bpots">
+        <div class="card  " style="width: 18rem;">
+          <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
+          <div class="card-body ">
+            <h4> Biriyani </h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+            <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="filterDiv lpack">
+        <div class="card  " style="width: 18rem;">
+          <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
+          <div class="card-body ">
+            <h4> Lunch Pack </h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+            <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
+          </div>
+        </div>
+      </div>
+      <div class="filterDiv lpack">
+        <div class="card  " style="width: 18rem;">
+          <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
+          <div class="card-body ">
+            <h4> Lunch Pack </h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+            <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
+          </div>
+        </div>
+      </div>
+      <div class="filterDiv bpots">
+        <div class="card  " style="width: 18rem;">
+          <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
+          <div class="card-body ">
+            <h4> Italian pizza </h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+            <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="filterDiv mutt">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
+          <div class="card-body ">
+            <h4> Italian pizza </h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+            <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="filterDiv bev">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
+          <div class="card-body ">
+            <h4> Italian pizza </h4>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+            <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
+          </div>
+        </div>
+
+      </div>
 
 
-<div id="myBtnContainer" my-5 >
-  <button class="btn active" onclick="filterSelection('all')"> Show all</button>
-  <button class="btn" onclick="filterSelection('bpots')"> Biriyani Pots</button>
-  <button class="btn" onclick="filterSelection('lpack')"> Lunch Pack</button>
-  <button class="btn" onclick="filterSelection('mutt')"> Mutton Masala</button>
-  <button class="btn" onclick="filterSelection('bev')"> Bevarages</button>
+
+   
+  
 </div>
 
-</div>
+<!--  -->
 
-<!-- The filterable elements. Note that some have multiple class names (this can be used if they belong to multiple categories) -->
+
 <div class="container-fluid">
- 
 
-  <div class="filterDiv bpots">
-    <div class="card bpots " style="width: 18rem;" >
-  <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
-  <div class="card-body ">
-  <h4> Biriyani </h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-    <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
-  </div>
-</div>
-
-</div>
-
-  <div class="filterDiv colors bpots"><div class="card  " style="width: 18rem;" >
-  <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
-  <div class="card-body ">
-  <h4> Biriyani </h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-    <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
-  </div>
-</div>
-</div>
-
-  <div class="filterDiv lpack"><div class="card  " style="width: 18rem;" >
-  <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
-  <div class="card-body ">
-  <h4> Lunch Pack </h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-    <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
-  </div>
-</div>
-</div>
-<div class="filterDiv lpack"><div class="card  " style="width: 18rem;" >
-  <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
-  <div class="card-body ">
-  <h4> Lunch Pack  </h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-    <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
-  </div>
-</div>
-</div>  <div class="filterDiv bpots"><div class="card  " style="width: 18rem;" >
-  <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
-  <div class="card-body ">
-  <h4> Italian pizza </h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-    <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
-  </div>
-</div>
-</div>
-
-  <div class="filterDiv mutt"><div class="card" style="width: 18rem;" >
-  <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
-  <div class="card-body ">
-  <h4> Italian pizza </h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-    <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
-  </div>
-</div>
-</div>
-
-  <div class="filterDiv bev"> <div class="card" style="width: 18rem;" >
-  <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
-  <div class="card-body ">
-  <h4> Italian pizza </h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-    <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
-  </div>
-</div>
-
-</div>
-<div class="filterDiv bev"> <div class="card" style="width: 18rem;" >
-  <img class="card-img-top" src="assets/images/brijpg.jpg" alt="Card image cap">
-  <div class="card-body ">
-  <h4> Italian pizza </h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-    <a href="#" class="theme-btn">Order Now <i class="fa fa-arrow-right"> </i> </a>
-  </div>
-</div>
-
-</div>
-
-</div>
-
-</div>
-
-
-
-<!-- -------------------------------------------------------------------------------------------------------------------------- -->
-
-<!------------------------------------------------CUSTOMER REVIEW ------------------------------------------------------------------>
-
-
-
-
-<div class=" text-center heading-section ftco-animate  my-5">
+<div class=" text-center heading-section ftco-animate   my-5">
     <p class=" brushfont">Testimonial</p>
 </div>
 
@@ -424,6 +309,9 @@ include_once "nav.php";
     <span class="sr-only">Next</span>
   </a>
 </div>
+
+
+</div>
 <!----------------------------------contact us ------------------------------------------------------------------------------------->
 <section class="ftco-section "data-aos="fade-up" style="background-color: black;">
 		<div class="container">
@@ -523,119 +411,3 @@ include_once "nav.php";
 			</div>
 		</div>
 	</section>
-
-
-
-  <!--  -->
-<?php
-include_once "footer.php";
-?>
-<style>
-.containermenue {
- overflow: hidden; 
-/* padding:50px; */
-
-
-
-}
-
-.filterDiv {
-  float: left;
-  /* background-color: #2196F3;
-  color: #ffffff; */
-  /* width: 100px; */
-  /* line-height: 100px;
-  text-align: center; */
-  /* margin: 2px; */
-  /* display: none; */
-  
-  /* Hidden by default */
-  /* padding: auto; */
-
- 
-}
-
-/* The "show" class is added to the filtered elements */
-.show {
-  display: block;
-}
-
-/* Style the buttons */
- .btn {
-  /* border: none; */
-  /* outline: 2px;
-  outline-color: black; */
-  padding: 2px 5px;
-background-color: #deb150; 
-  color: black;
-  cursor: pointer;
-  outline: 1px solid black;
-    outline-offset: -5px;
-    outline-color: black;
-} 
-
-/* Add a light grey background on mouse-over */
-.btn:hover {
-  background-color: black;
-  color: white;
-} 
-
-/* Add a dark background to the active button */
-/* .btn.active {
-  background-color: #666;
-  color: white;
-/* }  */
-</style>
-
-
-
-<script>
-  filterSelection("all")
-
-  function filterSelection(c) {
-    var x, i;
-    x = document.getElementsByClassName("filterDiv");
-    if (c == "all") c = "";
-    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-    for (i = 0; i < x.length; i++) {
-      w3RemoveClass(x[i], "show");
-      if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-    }
-  }
-
-  // Show filtered elements
-  function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) {
-        element.className += " " + arr2[i];
-      }
-    }
-  }
-
-  // Hide elements that are not selected
-  function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-        arr1.splice(arr1.indexOf(arr2[i]), 1);
-      }
-    }
-    element.className = arr1.join(" ");
-  }
-
-  // Add active class to the current control button (highlight it)
-  var btnContainer = document.getElementById("myBtnContainer");
-  var btns = btnContainer.getElementsByClassName("btn");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    });
-  }
-</script>
--- nav.php
+++ nav.php
@@ -15,7 +15,7 @@
         <a class="nav-link" href="#menu">Menu</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link " href="#">Contacts</a>
       </li>
     </ul>
     <form class="my-2 my-lg-0">
